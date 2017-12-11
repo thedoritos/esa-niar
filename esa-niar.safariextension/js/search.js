b6ff88563400b1dfe5a56c0b6863376d27f254fd
thedoritos <thedoritos@gmail.com>
2017-12-12 06:08:19 +0900

    :see_no_evil: Show results

--- esa-niar.safariextension/js/search.js
+++ esa-niar.safariextension/js/search.js
@@ -1,6 +1,27 @@
 safari.self.addEventListener('message', function(event) {
   if (event.name === 'searchResult') {
-    console.log(event.message);
+    var container = document.getElementById('rhs_block');
+    if (container == null) {
+      return;
+    }
+
+    var header = document.createElement('div');
+    header.innerHTML = '<h2>Esa Results:</h2>';
+    container.insertBefore(header, container.children[0])
+
+    var result_list = document.createElement('ul');
+    result_list.setAttribute('width', '200px');
+    result_list.setAttribute('height', '640px');
+
+    var results = JSON.parse(event.message).posts;
+    results.forEach(p => {
+      var item = document.createElement('li');
+      item.innerHTML = '<a href=' + p.url + '>' + p.full_name + '</a>';
+      result_list.appendChild(item);
+    });
+
+    container.insertBefore(result_list, container.children[1]);
+
     return;
   }
 
