99b9fbc7bab92138550bbd7abe225cc7da0d80dc
thedoritos <thedoritos@gmail.com>
2017-12-12 03:29:14 +0900

    Wait & insert fake list

--- esa-niar.safariextension/js/search.js
+++ esa-niar.safariextension/js/search.js
@@ -1,1 +1,18 @@
-console.log(new Date());
+var polling = window.setInterval(function() {
+  var container = document.getElementById('rhs_block');
+  if (container == null) {
+    return;
+  }
+
+  window.clearInterval(polling);
+  
+  var result_list = document.createElement('div');
+  result_list.innerHTML = "<ul>" + 
+    "<li>ONE</li>" +
+    "<li>TWO</li>" +
+    "<li>THREE</li>" +
+    "</ul>";
+  
+  console.log(container);
+  container.insertBefore(result_list, container.children[0]);
+}, 100);
